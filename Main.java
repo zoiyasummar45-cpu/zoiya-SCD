581d383b6cddae07420ff8d1e5db7af161654869
zoiyasummar45-cpu <zoiyasummar45@gmail.com>
2026-03-16 20:55:56 +0500

    Update

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Students s1 = new Students();
+        s1.name = "Zoiya";
+        s1.rollNumber = 5027;
+
+        Students s2 = new Students();
+        s2.name = "Summar ";
+        s2.rollNumber = 5059;
+
+        s1.displayinfo();
+        s2.displayinfo();
+    }
+
+    
+}
---
+++ Students.java
@@ -0,0 +1,15 @@
+public class Students {
+    String name;
+    int rollNumber;
+
+    public void displayinfo(){
+       
+        System.out.println("Student name is:" + name);
+        System.out.println("Student roll number is:" + rollNumber);
+
+        System.out.println("---------------------------");
+    }
+
+}
+
+
